cc75d3c3640032cddc9fbe0bcbc87a4dac009c36
Darshan <darshan922000@gmail.com>
2025-01-11 20:45:05 -0500

    logging and exception handling completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -14,13 +14,7 @@ class NetworkSecurityExcption(Exception):
         return "Error occured in python script name [{0}] line number [{1}], error message [{2}]".format(
         self.file_name, self.lineno, str(self.error_message))
 
-if __name__ == "__main__":
-    try:
-        logging.info("Entered into try block...!!!")
-        a = 1/0
-        print("This will not be printed", a)
-    except Exception as e:
-        raise NetworkSecurityExcption(e, sys)
+
         
         
 
